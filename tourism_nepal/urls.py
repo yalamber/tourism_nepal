9a7c62fdc56cf29b80c2ddd554ba4fc0dc7b5dea
Animesh Timsina <antim.timsina7777@gmail.com>
2019-06-17 19:04:21 +0545

    Attempt3

--- tourism_nepal/urls.py
+++ tourism_nepal/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from .schema import schema
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
