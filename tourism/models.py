d5cf2a3d6f5618b7e5ebedbf07fd149d798edb2f
Animesh Timsina <antim.timsina7777@gmail.com>
2019-06-19 22:36:53 +0545

    New

--- tourism/models.py
+++ tourism/models.py
@@ -65,6 +65,7 @@ class pubs(models.Model):
     place=models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    phone = models.CharField(max_length=15,null=True)
 
     def __str__(self):
         return self.name
