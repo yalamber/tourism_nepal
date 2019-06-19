bdd7f396da75896dacbc748bcea27fb8f8dfcb70
Animesh Timsina <antim.timsina7777@gmail.com>
2019-06-19 20:12:53 +0545

    Festivals added

--- tourism/admin.py
+++ tourism/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import attractions
+from .models import attractions,festivals,hotels
 # Register your models here.
 admin.site.register(attractions)
+admin.site.register(festivals)
+admin.site.register(hotels)
--- tourism/models.py
+++ tourism/models.py
@@ -13,3 +13,28 @@ class attractions(models.Model):
 
     def __str__(self):
         return self.name
+
+class hotels(models.Model):
+    name=models.CharField(max_length=100)
+    district=models.CharField(max_length=100)
+    province=models.IntegerField(default=1)
+    place=models.CharField(max_length=100)
+    cost=models.FloatField()
+    description=models.CharField(max_length=10000)
+    amenities=ArrayField(models.CharField(max_length=10000, blank=True))
+    url=models.URLField()
+    book=models.URLField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class festivals(models.Model):
+    name=models.CharField(max_length=100)
+    image=models.CharField(max_length=200)
+    img=ArrayField(models.CharField(max_length=1000, blank=True))
+    description = ArrayField(models.CharField(max_length=10000, blank=True))
+
+    def __str__(self):
+        return self.name
